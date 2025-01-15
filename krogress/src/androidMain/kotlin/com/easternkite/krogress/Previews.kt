7b86651097b11622524466671a71ff779b3c3bab
Dong-Yeon Lee <easternkite7@gmail.com>
2025-01-15 23:26:31 +0900

    Add a modifier on PulsatingDot (#17)

--- krogress/src/androidMain/kotlin/com/easternkite/krogress/Previews.kt
+++ krogress/src/androidMain/kotlin/com/easternkite/krogress/Previews.kt
@@ -1,8 +1,12 @@
 package com.easternkite.krogress
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.easternkite.krogress.components.indicators.PacmanIndicator
+import com.easternkite.krogress.components.indicators.PulsatingDot
 import com.easternkite.krogress.components.indicators.SemiCircleSpinIndicator
 
 @Composable
@@ -15,4 +19,10 @@ fun PacmanIndicatorPreview() {
 @Preview
 fun LineSpinFadeLoaderIndicatorPreview() {
     SemiCircleSpinIndicator()
+}
+
+@Composable
+@Preview
+fun PulsatingDotPreview() {
+    PulsatingDot(modifier = Modifier.size(200.dp), dotsCount = 3, animationDuration = 600)
 }
